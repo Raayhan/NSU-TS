145921da6cb62e84ce8cbcb182f6130222490f6e
nehal-ahomed <52578984+nehal-ahomed@users.noreply.github.com>
2019-12-08 16:24:02 +0600

    Add files via upload

--- nsu-ts/sql/nsu-ts.sql
+++ nsu-ts/sql/nsu-ts.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2019 at 05:58 PM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.1.12
+-- Generation Time: Dec 08, 2019 at 10:46 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `nsu-ts`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` int(11) NOT NULL,
+  `phone` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `first_name`, `email`, `password`, `phone`) VALUES
+(1, 'Nehal', 'nehal@gmail.com', 1234, 1761415961);
 
 -- --------------------------------------------------------
 
@@ -45,9 +66,35 @@ CREATE TABLE `students` (
 
 INSERT INTO `students` (`id`, `first_name`, `last_name`, `email`, `password`, `phone`, `gender`, `department`) VALUES
 (1, 'Rayhan', 'Rakib', 'rayhan.rakib@northsouth.edu', '123', 1717273999, 'male', 'ece'),
-(2, 'Fahmida', 'Prity', 'fahmida.prity@gmail.com', '123', 1523441812, 'female', 'pharm'),
-(3, 'Fahmida', 'Prity', 'fahmida.prity@gmail.com', '123', 157895412, 'female', 'arch'),
-(4, 'Ahmed', 'Rakib', 'rakib.nsu.cse@gmail.com', '858', 169851336, 'male', 'bba');
+(4, 'Ahmed', 'Rakib', 'rakib.nsu.cse@gmail.com', '858', 169851336, 'male', 'bba'),
+(5, 'Nehal', 'Ahmed', 'nehal@gmail.com', '123', 1768221478, 'Male', 'ECE'),
+(6, 'Nehal', 'Ahomed', 'nehalkhan.555@hotmail.com', '123', 123456789, 'Male', 'ECE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subjects`
+--
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `e_mail` varchar(100) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `department` varchar(100) NOT NULL,
+  `subject_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subjects`
+--
+
+INSERT INTO `subjects` (`id`, `first_name`, `last_name`, `e_mail`, `phone`, `gender`, `department`, `subject_name`) VALUES
+(7, 'Nehal', 'Ahmed', 'nehal@hotmail.com', 24325235, 'Male', 'ECE', 'cse311'),
+(8, 'n', 'a', 'ne@g.vom', 214214, 'Male', 'Architecture', 'arc211'),
+(9, 'p', 'q', 'ne@g.vom', 322, 'Male', 'Architecture', 'cse311');
 
 -- --------------------------------------------------------
 
@@ -71,16 +118,29 @@ CREATE TABLE `teachers` (
 --
 
 INSERT INTO `teachers` (`id`, `first_name`, `last_name`, `email`, `password`, `phone`, `gender`, `department`) VALUES
-(1, 'Rayhan', 'Rakib', 'rakib.nsu.cse@gmail.com', '125', 1675613100, 'male', 'ece');
+(1, 'Rayhan', 'Rakib', 'rakib.nsu.cse@gmail.com', '125', 1675613100, 'male', 'ece'),
+(2, 'Nehal', 'Ahmed', 'nehal@gmail.com', '123', 1768221478, 'Male', 'ECE');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -94,16 +154,28 @@ ALTER TABLE `teachers`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
